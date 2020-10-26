a6175d1daab41b8b4abec0ec38ca5be22be63110
Skad0sh <57108056+Skad0sh@users.noreply.github.com>
2020-10-27 01:03:42 +0530

    Update main.php

--- main.php
+++ main.php
@@ -2,39 +2,46 @@
 // Start the session
 session_start();
 include_once "dbh.php";
+if($_SESSION["user"]==""){
+	header("Location: http://localhost:4000/Desktop/php/index.php?You-are-not-logged-in");
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Test PHP Advanced</title>
+	<title>PHP WebCart</title>
 	<link href="style.css" rel="stylesheet">
 </head>
 <body>
-	<?php
+	<h1 class = "titles">Welcome to WebCart, <?php echo $_SESSION["user"]; ?> </h1>
 
-		echo "<h1>FFFFFFFFFFFFFFFFFF it worked, Welcome ".$_SESSION["user"]."</h1>";
-		echo session_id()."<br>";
-		$query = "SELECT * FROM users;";
+	<?php
+		$query = "SELECT*FROM products;";
 		$resultSet = mysqli_query($conn,$query);
-		$resultCheck = mysqli_num_rows($resultSet);
-		if($resultCheck>0){
-			while($row = mysqli_fetch_assoc($resultSet)){
-				echo $row["user"]."<br>";
-				echo $row["password"];
-			}
+		while($row = mysqli_fetch_assoc($resultSet)){
+			$image = $row["proLink"];
+			$name = $row["proName"];
+			$price = $row["proPrice"];
+			echo "<div>";
+			echo "<table>";
+			echo "<tr>";
+			echo "<td><img src=".$image."></td>";
+			echo "<td><p style='font-size:40px'>".$name."</p><br></td><br>";
+			echo "</tr>";
+			echo "</table>";
+			echo "</div>";
 		}
-		else{
-			echo "resultSet is Empty";
-		}
-		#$myfile = fopen("test.txt","w") or die("Unable to open file!");
-		#$txt = "Mickey Mouse\n";
-		#fwrite($myfile, $txt);
-		#echo fread($myfile,filesize("test.txt"));
-		#$txt = "Minnie Mouse\n";
-		#fwrite($myfile, $txt);
-		#echo fread($myfile,filesize("test.txt"));
-		#fclose($myfile);
-		
 	?>
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+		<select id="qty1" name="qty1">
+			<option value='1'>1</option>
+			<option value='2'>2</option>
+			<option value='3'>3</option>
+			<option value='4'>4</option>
+			<option value='5'>5</option>
+		</select>
+		<label class="labels" name="add2cart1">Add to cart</label>
+		<input type="checkbox" name="item1" id="item1" value="Add to cart" for="add2cart1">
+	</form>
 </body>
 </html>
