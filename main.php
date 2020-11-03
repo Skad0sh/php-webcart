b43e0f182d5eb356bdd9e261d46bb4da2e238aa1
Skad0sh <57108056+Skad0sh@users.noreply.github.com>
2020-11-03 16:43:11 +0530

    Update main.php

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 // Start the session
 session_start();
 include_once "dbh.php";
-if($_SESSION["user"]==""){
+if($_SESSION["user"]===""){
 	header("Location: http://localhost:4000/Desktop/php/index.php?You-are-not-logged-in");
 }
 ?>
@@ -13,35 +13,77 @@ if($_SESSION["user"]==""){
 	<link href="style.css" rel="stylesheet">
 </head>
 <body>
+	<div style="margin-left: 400px;margin-right: 400px;">
 	<h1 class = "titles">Welcome to WebCart, <?php echo $_SESSION["user"]; ?> </h1>
+	</div>
+	<div style="margin-left: 400px;margin-right: 400px;">
+	<button class="nav-button" onclick="window.location.assign('http://localhost:4000/Desktop/php/cart.php')">Buy Cart🛒</button>
+	<button class="nav-button" onclick="window.location.assign('http://localhost:4000/Desktop/php/admin.php')">Admin CP</button>
+	<button class="nav-button" onclick="window.location.assign('http://localhost:4000/Desktop/php/')">Logout</button>
+</div>
+	<hr>
 
 	<?php
 		$query = "SELECT*FROM products;";
 		$resultSet = mysqli_query($conn,$query);
+		if(!isset($cartList)){
+			$cartList=array();
+			#echo "check1";
+		}
+	?>
+		<form method = "POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" id="proForm">
+	<?php
 		while($row = mysqli_fetch_assoc($resultSet)){
 			$image = $row["proLink"];
 			$name = $row["proName"];
 			$price = $row["proPrice"];
+			$sn = $row["Sn"];
 			echo "<div>";
-			echo "<table>";
+			echo "<table style='border-spacing: 30px;'>";
 			echo "<tr>";
 			echo "<td><img src=".$image."></td>";
-			echo "<td><p style='font-size:40px'>".$name."</p><br></td><br>";
+			echo "<td><p style='font-size:40px'>".$name."</p><br><p style='font-size:20px'>Price: ".$price."</p></td>";
+			echo "<td><br><br><button class='cart-button' name='add-to-cart' type='submit' form='proForm' value=".$sn.">Add to cart🛒</button></td>";
 			echo "</tr>";
 			echo "</table>";
 			echo "</div>";
 		}
+		echo "</form>";
+		if($_SERVER["REQUEST_METHOD"]=="POST"){
+			$query = "SELECT*FROM products;";
+			$resultSet = mysqli_query($conn,$query);
+			#echo $_POST["Sn"];
+			while($row = mysqli_fetch_assoc($resultSet)){
+				if($row["Sn"] == $_POST["add-to-cart"]){
+					#$cartList[count($cartList)] = $row["proName"];
+					if(isset($_SESSION["cart"])){
+						$proID = array_column($_SESSION["cart"],"id");
+						if(!in_array($_POST["add-to-cart"],$proID)){
+							$count = count($_SESSION["cart"]);
+							$cartList = array(
+							"id" => $row["Sn"],
+							"product" => $row["proName"],
+							"price" => $row["proPrice"]
+							);
+							$_SESSION["cart"][$count] = $cartList;
+						}
+						else{
+							echo "<script>alert('Item is already in the cart!')</script>";
+						}
+					}
+					else{
+						$cartList = array(
+							"id" => $row["Sn"],
+							"product" => $row["proName"],
+							"price" => $row["proPrice"]
+						);
+						$_SESSION["cart"][0] = $cartList;
+
+					}
+
+				}
+			}
+		}
 	?>
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-		<select id="qty1" name="qty1">
-			<option value='1'>1</option>
-			<option value='2'>2</option>
-			<option value='3'>3</option>
-			<option value='4'>4</option>
-			<option value='5'>5</option>
-		</select>
-		<label class="labels" name="add2cart1">Add to cart</label>
-		<input type="checkbox" name="item1" id="item1" value="Add to cart" for="add2cart1">
-	</form>
 </body>
 </html>
